6e87b5fb102441957faa9b3bffda361e7fa54157
Halasson <gtm24halas@gmail.com>
2016-10-07 18:05:25 +0700

    Initial commit

---
+++ app/src/main/java/com/gtm/halasson/gtmgit/GTMgitmainAct.java
@@ -0,0 +1,13 @@
+package com.gtm.halasson.gtmgit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GTMgitmainAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gtmgitmain);
+    }
+}
